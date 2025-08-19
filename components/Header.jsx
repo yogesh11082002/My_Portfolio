eb6a57d28b86eb7d2171d7085bee0c3071f4363c
Yogesh <yogeshthakur938920@gmail.com>
2025-08-19 09:12:16 +0530

    add padding for phone

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,90 +1,4 @@
-// import { assets } from "@/assets/assets";
-// import Image from "next/image";
-// import React from "react";
-// import { useContext } from "react";
-// import { ThemeContext } from "@/app/layout";
-// import { motion } from "motion/react";
 
-// const Header = () => {
-//   const { isDarkMode } = useContext(ThemeContext);
-//   return (
-//     <div className="w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-center gap-3 pt-0 lg:pt-16 ">
-//       <motion.div
-//         initial={{ scale: 0 }}
-//         whileInView={{ scale: 1 }}
-//         transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
-//       >
-//         <Image src={assets.profile_img} alt="" className="rounded-lg w-50" />
-//       </motion.div>
-//       <motion.h3
-//         initial={{ y: -20, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//         transition={{ duration: 0.6, delay: 0.3 }}
-//         className="flex items-end gap-2  text-xl md:text-2xl  mb-3 font-ovo"
-//       >
-//         Hi! I'm Yogesh Sengar
-//         <Image src={assets.hand_icon} alt="" className=" w-6" />
-//       </motion.h3>
-//       <motion.h1
-//         initial={{ opacity: 0 }}
-//         whileInView={{ opacity: 1 }}
-//         transition={{ duration: 0.6, delay: 0.7 }}
-//         className="text-3xl sm:text-6xl lg:text-[50px] font-ovo"
-//       >
-//         Full Stack web developer{" "}
-//       </motion.h1>
-//       <motion.p
-//         initial={{ y: -20, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//         transition={{ duration: 0, delay: 0.3 }}
-//         className="max-w-2xl  mx-auto font-ovo"
-//       >
-//         {" "}
-//         I am a passionate full stack developer skilled in building dynamic,
-//         scalable web applications using modern technologies and frameworks.
-//       </motion.p>
-//       <div className="flex flex-col sm:flex-row items-center  gap-4 mt-4">
-//         <motion.a 
-//         initial={{ y:30, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//           transition={{ duration: 0.6 ,delay:1 }}
-//           href="#contact"
-//           className="flex items-center    gap-2 px-10 py-3 border border-white bg-black text-white rounded-full "
-//         >
-//           {" "}
-//           contact me
-//           <Image
-//             src={assets.right_arrow_white}
-//             alt="arrow icon"
-//             className="w-4"
-//           />
-//         </motion.a>
-//         <motion.a 
-//        initial={{ y:30, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//           transition={{ duration: 0.6 ,delay:1.2 }}
-//           href="/sample-resume.pdf"
-//           download
-//           className={`flex items-center gap-2 font-ovo px-10 py-3 border rounded-full transition-colors duration-300 
-//     ${
-//       isDarkMode
-//         ? "bg-white text-black border-white"
-//         : "border-gray-500 bg-transparent text-black"
-//     }`}
-//         >
-//           my resume
-//           <Image
-//             src={assets.download_icon}
-//             alt="arrow icon"
-//             className={`w-4 `}
-//           />
-//         </motion.a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
 
 import { assets } from "@/assets/assets";
 import Image from "next/image";
@@ -96,7 +10,7 @@ const Header = () => {
   const { isDarkMode } = useContext(ThemeContext);
 
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-center gap-3 pt-0 lg:pt-24 ">
+    <div className="w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-center gap-3 pt-12 lg:pt-24 ">
 
       {/* Profile Image */}
       <motion.div
